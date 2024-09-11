5ab28eca9f40416b1230c8c3dc863d402726fe36
Oscar <oscarcr189@outlook.com>
2024-09-10 19:53:38 -0600

    creando proyecto

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'cart-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart-app.component.html'
+})
+export class CartAppComponent {
+
+}
