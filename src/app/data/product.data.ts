b86de736107102d99c89d15ddd0c77de8e5deb4f
Oscar <oscarcr189@outlook.com>
2024-09-10 20:24:57 -0600

    datos de la carpeta data

---
+++ src/app/data/product.data.ts
@@ -0,0 +1,56 @@
+
+export const product:Product [] = [
+  {
+
+    id:1,
+    name:"teclado mecanico AsusRGB",
+    descripcion:"Algun teclado mecanico con luces",
+    price:1000
+
+  },
+  {
+
+    id:2,
+    name:"Samsung Smart TV LED 75'",
+    descripcion:"Algun LCD excelente",
+    price:17000
+
+  },
+  {
+
+    id:3,
+    name:"Soni Camara VIdeo",
+    descripcion:"Algun camara de video para streaming",
+    price:1700
+
+  },
+  {
+
+    id:4,
+    name:"Sony camara video",
+    descripcion:"Alguna camara de video para stremear",
+    price:3100
+
+  },
+
+  {
+
+    id:5,
+    name:"Nvidia ASUS RTX4900",
+    descripcion:"TArgeta de video Nvidia optimizada para tareas multicore",
+    price:5000
+
+  },
+  {
+
+    id:6,
+    name:"CPU Intel core i7",
+    descripcion:"CPU optimizadas para tareas multicor",
+    price:4000
+
+  },
+
+
+
+]
+
---
+++ src/app/models/product.ts
@@ -0,0 +1,8 @@
+
+export class Product {
+  id: number =0;
+  name:string="";
+  descripcion:string="";
+  price:number = 0;
+  
+}
