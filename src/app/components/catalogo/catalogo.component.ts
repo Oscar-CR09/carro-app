84338ff3537bad1c3f1da6384b240f97d53d777a
Oscar <oscarcr189@outlook.com>
2024-09-11 21:49:26 -0600

    catalogo1

---
+++ src/app/components/catalogo/catalogo.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'catalogo',
+  standalone: true,
+  imports: [ProductCardComponent],
+  templateUrl: './catalogo.component.html'
+})
+export class CatalogoComponent {
+  @Input() products!:Product[];
+
+}
