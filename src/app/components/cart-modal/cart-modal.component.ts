b02222c6bc666df70b5e396fe92136ab520eaf00
Oscar <oscarcr189@outlook.com>
2024-10-06 22:35:34 -0600

    Modal component

---
+++ src/app/components/cart-modal/cart-modal.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CartComponent } from '../cart/cart.component';
+import { CartItem } from '../../models/cartItem';
+
+@Component({
+  selector: 'app-cart-modal',
+  standalone: true,
+  imports: [CartComponent],
+  templateUrl: './cart-modal.component.html'
+})
+export class CartModalComponent {
+
+  @Input() items: CartItem[] =[];
+
+  @Input() total =0;
+
+  @Output() idProductEventEmiter=new EventEmitter();
+
+  @Output() closeEventEmitter =new EventEmitter();
+
+  onDeleteCart(id:number){
+    this.idProductEventEmiter.emit(id);
+
+  }
+
+  closeCart():void {
+    this.closeEventEmitter.emit();
+
+  }
+
+}
